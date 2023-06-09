c6caabdb3d84c73c982411601f578e4de05b447d
hamedakbarii <hamedkarate0@gmail.com>
2023-06-09 22:23:57 +0430

    router updated

--- react_project/src/Components/RouterBar/Routerbar.jsx
+++ react_project/src/Components/RouterBar/Routerbar.jsx
@@ -11,28 +11,40 @@ export default function RouterBar() {
   return (
     <section className="bg-gray-50 w-full shadow-lg">
       <ul className="p-2 flex justify-start items-center md:items-start w-full gap-6">
-        <NavLink activeClassName="active" to="/" className="w-80px">
-          <li className="flex justify-start items-center gap-2 p-2  transition-all">
+        <NavLink activeClassName="active" to="/" className="w-80px rounded-lg">
+          <li className="flex justify-start items-center gap-2 p-2 transition-all">
             <LineStyle />
             Home
           </li>
         </NavLink>
 
-        <NavLink to="/layout" activeClassName="active" className="w-80px">
+        <NavLink
+          to="/layout"
+          activeClassName="active"
+          className="w-80px rounded-lg"
+        >
           <li className="flex justify-start items-center gap-2 p-2  transition-all">
             <AttachMoney />
             Layout
           </li>
         </NavLink>
 
-        <NavLink to="/table" activeClassName="active" className="w-80px">
+        <NavLink
+          to="/table"
+          activeClassName="active"
+          className="w-80px rounded-lg"
+        >
           <li className="flex justify-start items-center gap-2 p-2  transition-all">
             <BarChart />
             Table
           </li>
         </NavLink>
 
-        <NavLink to="/searchbar" activeClassName="active" className="w-80px">
+        <NavLink
+          to="/searchbar"
+          activeClassName="active"
+          className="w-80px rounded-lg"
+        >
           <li className="flex justify-start items-center gap-2 p-2  transition-all">
             <DynamicFeed />
             SearchBar

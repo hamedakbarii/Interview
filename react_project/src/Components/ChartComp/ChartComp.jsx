2ff50b49f1b135eaaca06ea41c4f4b8ea5230ed6
hamedakbarii <hamedkarate0@gmail.com>
2023-06-14 03:20:33 +0330

    searchbar component updated and chart component added

--- react_project/src/Components/ChartComp/ChartComp.jsx
+++ react_project/src/Components/ChartComp/ChartComp.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
 
-export default function ChartComp() {
+export default function ChartComp({ showData }) {
+  let codes = showData.map((country) => country.item.code);
+  const [countryCode, setCountryCodes] = useState(codes);
+  const [apiData, setApiData] = useState();
+
+  const { isLoading, error } = useQuery(
+    ["myData", setCountryCodes],
+    async () => {
+      const response = await fetch(
+        `http://localhost:3000/chart/${countryCode}`
+      );
+      const data = await response.json();
+      setApiData(data.data.results);
+    },
+    {
+      enabled: Boolean(countryCode),
+    }
+  );
+  console.log(apiData);
+
   return <div>ChartComp</div>;
 }
--- react_project/src/Pages/Searchbar.jsx
+++ react_project/src/Pages/Searchbar.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { useQuery } from "react-query";
-// import ChartComp from "../components/ChartComp/ChartComp";
+import ChartComp from "../components/ChartComp/ChartComp";
 
 export default function Searchbar() {
-  const [searchQuery, setSearchQuery] = useState("ir");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showData, setShowData] = useState([]);
+  const inputRef = useRef(null);
 
-  const { data, isLoading, error } = useQuery("myData", () => {
-    return fetch(`http://localhost:3000/?q=${searchQuery}`);
-  });
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [showData]);
+
+  const { isLoading, error } = useQuery(
+    ["myData", searchQuery],
+    async () => {
+      const response = await fetch(`http://localhost:3000/?q=${searchQuery}`);
+      const data = await response.json();
+      setShowData(data.data.results);
+    },
+    {
+      enabled: Boolean(searchQuery),
+    }
+  );
 
   if (isLoading) {
     return <p className="text-center">Loading...</p>;
@@ -24,16 +38,26 @@ export default function Searchbar() {
         <input
           type="text"
           placeholder="Type Country Name..."
-          className="outline-none w-full"
+          className="outline-none w-full bg-transparent"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          ref={inputRef}
         />
         <KeyboardArrowDownIcon className="text-gray-500" />
       </div>
 
-      <div>test</div>
-
-      {/* <div>
-        <ChartComp countryCode={searchQuery} />
-      </div> */}
+      {showData && showData.length !== 0 && (
+        <div className="flex flex-col justify-center items-center w-full gap-16">
+          <select className="border p-2 border-gray-400 rounded-lg">
+            {showData.map((country) => (
+              <option key={country.item.code}>{country.item.name}</option>
+            ))}
+          </select>
+          <div>
+            <ChartComp showData={showData} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
